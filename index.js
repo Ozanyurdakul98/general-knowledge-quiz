1c754242f92832b48f13c690ba10f76e3f915b0e
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-09-08 11:47:46 +0200

    added ad created Card when clicking Contribute Form Button functionality

--- index.js
+++ index.js
@@ -29,17 +29,52 @@ const quizQuestion_p = document.querySelector('[data-js="quizQuestion-p"]');
 const quizQuestionInput = document.querySelector(
   '[data-js="quizQuestionInput"]'
 );
-quizQuestionInput.addEventListener("input", (event) => {
+//
+const tag_a = document.querySelector("[data-js='tagA']");
+const newTagInput = document.querySelector("[data-js='newTagInput']");
+const tag_a2 = document.querySelector("[data-js='tagA2']");
+const newTagInput2 = document.querySelector("[data-js='newTagInput2']");
+const tag_a3 = document.querySelector("[data-js='tagA3']");
+const newTagInput3 = document.querySelector("[data-js='newTagInput3']");
+const tag_a4 = document.querySelector("[data-js='tagA4']");
+const newTagInput4 = document.querySelector("[data-js='newTagInput4']");
+//
+const createdCard = document.querySelector('[data-js ="createdCard"]');
+const cardsSection = document.querySelector('[data-js="cardsSection"]');
+//
+//
+quizQuestionInput.addEventListener("input", () => {
   quizQuestion_p.textContent = quizQuestionInput.value;
 });
-quizAnswerInput.addEventListener("input", (event) => {
-  console.log(quizAnswerInput.value);
+quizAnswerInput.addEventListener("input", () => {
   quizAnswer_p.textContent = quizAnswerInput.value;
+});
+newTagInput.addEventListener("input", () => {
+  tag_a.textContent = newTagInput.value;
+});
+newTagInput2.addEventListener("input", () => {
+  tag_a2.textContent = newTagInput2.value;
+});
+newTagInput3.addEventListener("input", () => {
+  tag_a3.textContent = newTagInput3.value;
+});
+newTagInput4.addEventListener("input", () => {
+  tag_a4.textContent = newTagInput4.value;
 });
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   form.reset();
   form.quizQuestion.focus();
+  //
+  //cardsSection.append(createdCard.value);
+  //
+  cardsSection.append(createdCard.cloneNode(true));
+  // const cardHTML = createdCard.innerHTML;
+  // cardsSection.insertAdjacentHTML(
+  //   "beforeend",
+  //   `<div class="card-section__card" data-js="createdCard">${cardHTML}</div>`
+  // );
+  //
   console.log("contributed");
 });
